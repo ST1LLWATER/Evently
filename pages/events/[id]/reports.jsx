dc0a74a5034fcad9aff95af0bb2d93f94beba89b
st1llwater <alokpunjofficial@gmail.com>
2022-06-16 18:53:21 +0530

    Updates

--- pages/events/[id]/reports.jsx
+++ pages/events/[id]/reports.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { loadUserData } from '../../../redux/userSlice';
 import { useRouter } from 'next/router';
+import { Button } from '@mantine/core';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Reports = () => {
+  const [data, setData] = useState(null);
+  const [coordinators, setCoordinators] = useState([]);
   const router = useRouter();
 
-  console.log(router.query.id);
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state);
 
-  return <div>Reports</div>;
+  const fetchEventData = async () => {
+    dispatch(loadUserData());
+    const res = await fetch(`/api/event/${router.query.id}`);
+    const data = await res.json();
+
+    console.log(data);
+
+    setCoordinators(() => {
+      const [creator, creator_rollno] = data.creator.split(':');
+      creator_rollno.toUpperCase();
+      return [creator_rollno, ...data.managers];
+    });
+
+    // let parsedDate = new Date(data.scheduled_date);
+    // parsedDate = parsedDate.toLocaleDateString('en-US', options);
+    // data.scheduled_date = parsedDate;
+
+    setData(data);
+  };
+
+  useEffect(() => {
+    if (!router.query.id) return;
+    fetchEventData();
+  }, [router]);
+
+  return (
+    <div>
+      <div
+        className={`${
+          coordinators.includes(userData.roll_no.toUpperCase())
+            ? 'p-4 flex justify-end'
+            : 'hidden'
+        }`}
+      >
+        <Button className="bg-rose-600 hover:bg-rose-700">Add Report</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Reports;
--- pages/index.jsx
+++ pages/index.jsx
@@ -52,9 +52,9 @@ const LandingPage = () => {
         Now managing and co-ordination events made simpler like never before....
       </p>
       <div className="flex flex-col sm:flex-row text-lg md:text-3xl font-semibold text-white sm:w-1/3 gap-4 sm:gap-10">
-        <button className="rounded-lg py-2 px-4 sm:w-1/2 bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700">
+        {/* <button className="rounded-lg py-2 px-4 sm:w-1/2 bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700">
           Profile
-        </button>
+        </button> */}
         <button
           onClick={() => router.push('/events')}
           className="rounded-lg py-2 px-4 sm:w-1/2 bg-gradient-to-r from-pink-500 to-pink-600 hover:from-pink-600 hover:to-pink-700"
